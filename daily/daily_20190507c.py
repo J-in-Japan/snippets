da7589f5144f1066f7091921b6e7f72ce3e77ce6
J-in-Japan <48210764+J-in-Japan@users.noreply.github.com>
2019-05-07 17:26:01 +0900

    renamed class > lecture

--- daily/daily_20190507c.py
+++ daily/daily_20190507c.py
@@ -3,27 +3,27 @@
 # +1 room for each start and -1 for each end
 # max reached is required number of rooms
 
-classess = [ [13, 15], [11, 13], [4, 7], [1, 5], [3, 6] ]
+lectures = [ [13, 15], [11, 13], [4, 7], [1, 5], [3, 6] ]
 
 # construct a action list:
 # action[0] -> time of action
 # action[1] -> type of action (-1 for finish type, 1 for start type)
 
 actions = []
-for cla55 in classess:
-    actions.append([cla55[0], 1])
-    actions.append([cla55[1], -1])
+for lecture in lectures:
+    actions.append([lecture[0], 1])
+    actions.append([lecture[1], -1])
 
 actions.sort()
 # [[2, 1], [3, 1], [4, -1], [4, 1], [6, -1], [7, -1], [11, 1], [13, -1], [13, 1], [15, -1]]
 
-min_classrooms = 0
-curr_classrooms = 0
+max_rooms = 0
+curr_rooms = 0
 
 for action in actions:
-    curr_classrooms += action[1]
+    curr_rooms += action[1]
 
-    if curr_classrooms > min_classrooms:
-        min_classrooms = curr_classrooms
+    if curr_rooms > max_rooms:
+        max_rooms = curr_rooms
 
-print(min_classrooms)
+print(max_rooms)
