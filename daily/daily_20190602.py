e6e6d9d637c21c816e10f9f3906da71debc49444
J-in-Japan <48210764+J-in-Japan@users.noreply.github.com>
2019-06-05 16:00:12 +0900

    fix 602

--- daily/daily_20190602.py
+++ daily/daily_20190602.py
@@ -13,49 +13,6 @@ class tree:
         self.left = None
         self.right = None
 
-pre = ['a', 'b', 'd', 'e', 'c', 'f', 'g']
-ino = ['d', 'b', 'e', 'a', 'f', 'c', 'g']
-
-# root = pre[0]
-# left_side = []
-# right_side = []
-# root_index = 100
-# for i in ino:
-#     if ino[i] != root and i < root_index:
-#         left_side.append(ino[i])
-#     elif ino[i] == root:
-#         root_index = i
-#     else:
-#         right_side.append(ino[i])
-
-
-# def build_a_tree(pre: list, ino: list) -> tree:
-#     if len(pre) == 0:
-#         return None
-    
-#     root_val = pre[0]
-#     root_ind = ino.index(root_val)
-
-#     ino_left = ino[0:root_ind]
-#     ino_right = ino[root_ind:]
-
-#     root = tree(root_val)
-#     root.left = build_a_tree(pre[1:], )
-
-
-
-
-
-# def build_tree(ino, pre, ino_start, ino_end):
-#     if ino_start > ino_end:
-#         return None
-    
-#     # pick next node from left side of preorder
-#     n = tree(pre[0])
-
-#     for p in range(len(pre)):
-#         ino
-
 
 def build_tree(in_order, pre_order, in_strt, in_end): 
       
@@ -104,11 +61,16 @@ def print_inorder(node):
     print_inorder(node.right) 
       
 # Driver program to test above function 
-in_order = ['D', 'B', 'E', 'A', 'F', 'C'] 
-pre_order = ['A', 'B', 'D', 'E', 'C', 'F'] 
+# in_order = ['D', 'B', 'E', 'A', 'F', 'C'] 
+# pre_order = ['A', 'B', 'D', 'E', 'C', 'F'] 
+
+in_order = ['d', 'b', 'e', 'a', 'f', 'c', 'g']
+pre_order = ['a', 'b', 'd', 'e', 'c', 'f', 'g']
+
 # Static variable preIndex 
 build_tree.pre_index = 0
 root = build_tree(in_order, pre_order, 0, len(in_order)-1) 
   
 # test the build tree by priting Inorder traversal 
-print(f"Inorder traversal of the constructed tree is {print_inorder(root)}")
+print("Inorder traversal of the constructed tree is:")
+print_inorder(root)
